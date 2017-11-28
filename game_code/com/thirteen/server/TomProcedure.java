da262eec72e4e28036ed73c121665c4c3472a30c
vgafx <33317088+vgafx@users.noreply.github.com>
2017-11-28 11:17:49 +0100

    Update TomProcedure.java

--- game_code/com/thirteen/server/TomProcedure.java
+++ game_code/com/thirteen/server/TomProcedure.java
@@ -1,4 +1,4 @@
-package com.thirteen.server;
+package distributed.systems.core;
 /**
  * UNFINISHED- Meant as a building block.
  * Class representation of the whole TOM procedure.
@@ -20,10 +20,13 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.PriorityBlockingQueue;
 
 import distributed.systems.core.*;
+import distributed.systems.core.logger.Logger;
 
+
+//Have this class extend Thread??
 public class TomProcedure{
 	private final int timeout = 1000;
-	
+	Logger logger;
 	private String[][] listOfServersAndPorts;
 	private String localServerId;
 	
@@ -41,110 +44,154 @@ public class TomProcedure{
 		processQueue = new LinkedBlockingQueue<Message>();
 		unDeliverablesQueue = new  LinkedBlockingQueue<Message>();
 		executionQueue = new LinkedBlockingQueue<Message>();
+		logger = new Logger();
 		localServerId = serverId;
 	}
 	
 	//QUEUE METHODS
 	
-	
 	/**
-	 * Call on msg receive by a client, local Logical Clock should be adjusted
-	 * before passing it here.
+	 * The whole TOM process packed into a single method.
+	 * No return type 
 	 * @param msg
-	 * @param serverID
 	 * @param LC
 	 */
-	public void insertInProcessQ(Message msg, int LC) {
+	public void submitMsgToTOM(Message msg, int LC) {
 		msg.put("LC", LC);
-		msg.put("serverID", localServerId);
-		try {
-			processQueue.put(msg);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+		msg.put("serverID", localServerId);	
+		processQueue.add(msg);
+		//Should return and have a separate thread take over the rest of the process
+		//otherwise this blocks.
+		//Step 1: Multicast the timestamped message
+		//Step 2: Everyone stores the message as undeliverable
+		//Step 3: Wait for other processes to send their proposed timestamps
+		//Step 4: Gather the  timestamps values and figure out the MAX 
+		//Step 5: Multicast the MAX timestamp so that everyone can append it to the message
+		//Step 6: Mark the message as deliverable on each server, and pass it to the execution queue
+				
 	}
 	
+	
 	/**
-	 * 
+	 * Returns the first available (executable) action
+	 * @return msg
 	 */
-	public void processProcessQueueMessage(Message msg) {
-		if(!processQueue.isEmpty()) {
-			//Step 1: Multicast the timestamped message
-			//Step 2: Wait for other processes to send their proposed timestamps
-			//processQueue.remove();
-			insertInUnDeliverablesQueue(msg);
-			
+	public Message getExecutableMsgFromTOM() {
+		Message msg;
+		msg = executionQueue.remove(); //needs testing
+		logger.logMessage(msg);
+		return msg;
+	}
+	
+	
+	/**
+	 * Method for polling the execution queue for available actions.
+	 * @return
+	 */
+	public boolean isMessageAvailable() {
+		if(!executionQueue.isEmpty() && ((Integer)executionQueue.peek().get("isDeliverable"))==1) {
+			return true;
 		}else {
-			//zzzzzzz...
-			System.out.println("Process Q was empty on processProcessQueueMessage call..");
-		}
-		
-	}
-	
-	/**
-	 * 
-	 * @param msg
-	 * @throws InterruptedException 
-	 */
-	public void insertInUnDeliverablesQueue(Message msg){
-		msg.put("isDeliverable", 0);
-		try {
-			unDeliverablesQueue.put(msg);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
-	
-	/**
-	 * 
-	 */
-	public void processUnDeliverablesQueueMessage(Message msg) {
-		if(!unDeliverablesQueue.isEmpty()) {
-			
-			
-			//Before exiting mark the msg and add it to the exec Q
-			msg.put("isDeliverable", 1);
-			insertInExcecutionQueue(msg);
-		}else {
-			//zzzzzzz...
+			return false;
 		}	
 	}
 	
-	/**
-	 * 
-	 * @param msg
-	 */
-	public void insertInExcecutionQueue(Message msg) {
-		if ((Integer) msg.get("isDeliverable")==1) {
-			try {
-				executionQueue.put(msg);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}else {
-			System.out.println("insertInExcecutionQueue called with unDeliverable msg");
-		}
-	}
-	
-	/**
-	 * Takes the first Message in the queue, pops it and returns it
-	 * back so that it can be applied on the Battlefield. Can return
-	 * null if called while the Q is empty.
-	 */
-	public Message processExcecutionQueueMessage(Message msg) {
-		if(!executionQueue.isEmpty()) {
-			Message tempMsg = executionQueue.remove();
-			return tempMsg;
-		}else {
-			//zzzzzz
-			System.out.println("processExcecutionQueueMessage called while Q was empty");
-			return null;
-		}
-	}
-	
-	
+//	
+//	/**
+//	 * Call on msg receive by a client, local Logical Clock should be adjusted
+//	 * before passing it here.
+//	 * @param msg
+//	 * @param LC
+//	 */
+//	public void insertInProcessQ(Message msg, int LC) {
+//		msg.put("LC", LC);
+//		msg.put("serverID", localServerId);
+//		try {
+//			processQueue.put(msg);
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+//	}
+//	
+//	/**
+//	 * 
+//	 */
+//	public void processProcessQueueMessage(Message msg) {
+//		if(!processQueue.isEmpty()) {
+//			//Step 1: Multicast the timestamped message
+//			//Step 2: Wait for other processes to send their proposed timestamps
+//			//processQueue.remove();
+//			insertInUnDeliverablesQueue(msg);
+//			
+//		}else {
+//			//zzzzzzz...
+//			System.out.println("Process Q was empty on processProcessQueueMessage call..");
+//		}
+//		
+//	}
+//	
+//	/**
+//	 * 
+//	 * @param msg
+//	 * @throws InterruptedException 
+//	 */
+//	public void insertInUnDeliverablesQueue(Message msg){
+//		msg.put("isDeliverable", 0);
+//		try {
+//			unDeliverablesQueue.put(msg);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
+//	}
+//	
+//	/**
+//	 * 
+//	 */
+//	public void processUnDeliverablesQueueMessage(Message msg) {
+//		if(!unDeliverablesQueue.isEmpty()) {
+//			
+//			
+//			//Before exiting mark the msg and add it to the exec Q
+//			msg.put("isDeliverable", 1);
+//			insertInExcecutionQueue(msg);
+//		}else {
+//			//zzzzzzz...
+//		}	
+//	}
+//	
+//	/**
+//	 * 
+//	 * @param msg
+//	 */
+//	public void insertInExcecutionQueue(Message msg) {
+//		if ((Integer) msg.get("isDeliverable")==1) {
+//			try {
+//				executionQueue.put(msg);
+//			} catch (InterruptedException e) {
+//				e.printStackTrace();
+//			}
+//		}else {
+//			System.out.println("insertInExcecutionQueue called with unDeliverable msg");
+//		}
+//	}
+//	
+//	/**
+//	 * Takes the first Message in the queue, pops it and returns it
+//	 * back so that it can be applied on the Battlefield. Can return
+//	 * null if called while the Q is empty.
+//	 */
+//	public Message processExcecutionQueueMessage(Message msg) {
+//		if(!executionQueue.isEmpty()) {
+//			Message tempMsg = executionQueue.remove();
+//			return tempMsg;
+//		}else {
+//			//zzzzzz
+//			System.out.println("processExcecutionQueueMessage called while Q was empty");
+//			return null;
+//		}
+//	}
 	
 	
 	//Helper methods
@@ -161,6 +208,7 @@ public class TomProcedure{
 		}
 		return result;
 	}
+	
 	
 	
 	
