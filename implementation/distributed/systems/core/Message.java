1ca8015bdff37e9f1568a1f72fc276cdd3d05216
vgafx <33317088+vgafx@users.noreply.github.com>
2017-11-28 17:52:33 +0100

    Create Message.java

---
+++ implementation/distributed/systems/core/Message.java
@@ -0,0 +1,42 @@
+package distributed.systems.core;
+
+import java.util.HashMap;
+import java.io.Serializable;
+
+import distributed.systems.das.units.Unit;
+
+
+public class Message implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	HashMap<String,Serializable> contents;
+	
+	public Message(){
+		contents = new HashMap<String,Serializable>();
+	
+	}
+	
+	public void put(String key,Serializable value){
+		contents.put(key, value);
+	}
+	
+	public void put (String key, Unit value){
+		contents.put(key, value);
+	}
+	
+	public void put (String key, int value){
+		contents.put(key, value);
+	}
+	
+	public Serializable get(String key) {
+		return contents.get(key);
+	}
+	
+	
+	public String toString(){
+		return contents.toString();
+	}
+	
+	
+}
