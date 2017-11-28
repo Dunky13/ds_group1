8da34a405b51bacc36cdf922f378a090a9f3a17f
vgafx <33317088+vgafx@users.noreply.github.com>
2017-11-28 17:53:04 +0100

    Create Constants.java

---
+++ implementation/distributed/systems/core/Constants.java
@@ -0,0 +1,17 @@
+package distributed.systems.core;
+
+public final class Constants{
+	
+	public static final String HOSTNAME="localhost";
+	
+	public static int[] TOM_PORTS = {2000,2001,2002,2003,2004};
+	
+	
+	public static int[] RECEIVER_PORTS = {3000,3001,3002,3003,3004};
+	
+	public static int[] MULTICAST_PORTS = {4000,4001,4002,4003,4004};
+	
+	public static int[] PUBLISH_PORTS = {5000,5001,5002,5003,5004};
+	
+
+}
