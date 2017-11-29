4fc99470c5f10dcc4ed328b84cb0f472f0f25090
vgafx <33317088+vgafx@users.noreply.github.com>
2017-11-29 21:18:39 +0100

    Create Socket.java

---
+++ implementation/distributed/systems/core/Socket.java
@@ -0,0 +1,43 @@
+package distributed.systems.core;
+
+import distributed.systems.das.BattleField;
+import distributed.systems.das.units.Unit;;
+
+
+public class Socket {
+	
+	
+	
+	public Socket() {
+		
+	}
+
+
+	
+	public void unRegister() {
+		
+		
+	}
+
+	public void sendMessage(Message msg, String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addMessageReceivedHandler(BattleField battleField) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void register(String serverid) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addMessageReceiveHandler(Unit unit) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
