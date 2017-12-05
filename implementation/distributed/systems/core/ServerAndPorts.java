e7e14ef1a50f7006db6892800ff921aa1d618306
Marc Went <mwent@live.nl>
2017-12-05 13:43:57 +0100

    Minor refactoring

--- implementation/distributed/systems/core/ServerAndPorts.java
+++ implementation/distributed/systems/core/ServerAndPorts.java
@@ -1,7 +1,11 @@
 package distributed.systems.core;
 
-public class ServerAndPorts {
-	
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+
+public class ServerAndPorts
+{
+
 	private String hostname;
 	private int tom_port;
 	private int receiver_port;
@@ -9,72 +13,87 @@ public class ServerAndPorts {
 	private int publish_port;
 	private int id;
 
-	ServerAndPorts(int id, String hostname, int tom_port, int receiver_port, int multicast_port, int publish_port) {
+	ServerAndPorts(int id, String hostname, int tom_port, int receiver_port, int multicast_port, int publish_port)
+	{
 		this.id = id;
 		this.hostname = hostname;
 		this.tom_port = tom_port;
 		this.receiver_port = receiver_port;
 		this.multicast_port = multicast_port;
 		this.publish_port = publish_port;
-		
+
 	}
 
 	/**
 	 * @return the id
 	 */
-	public int getId() {
+	public int getID()
+	{
 		return id;
 	}
 
 	/**
 	 * @return the hostname
 	 */
-	public String getHostname() {
+	public String getHostname()
+	{
 		return hostname;
 	}
 
 	/**
 	 * @return the tom_port
 	 */
-	public int getTom_port() {
+	public int getTom_port()
+	{
 		return tom_port;
 	}
 
 	/**
 	 * @return the receiver_port
 	 */
-	public int getReceiver_port() {
+	public int getReceiver_port()
+	{
 		return receiver_port;
 	}
 
 	/**
 	 * @return the multicast_port
 	 */
-	public int getMulticast_port() {
+	public int getMulticast_port()
+	{
 		return multicast_port;
 	}
 
 	/**
 	 * @return the publish_port
 	 */
-	public int getPublish_port() {
+	public int getPublish_port()
+	{
 		return publish_port;
 	}
-	
-	public int getServerToServerReceivePort() {
+
+	public int getServerToServerReceivePort()
+	{
 		return publish_port;
 	}
-	
-	public int getServerToServerSendPort() {
+
+	public int getPort()
+	{
 		return multicast_port;
 	}
-	
-	public int getServerToClientPort() {
+
+	public int getServerToClientPort()
+	{
 		return receiver_port;
 	}
-	
-	public int getClientToServerPort() {
+
+	public int getClientToServerPort()
+	{
 		return tom_port;
 	}
 
+	public SocketAddress getSocketAddress()
+	{
+		return new InetSocketAddress(this.getHostname(), this.getPort());
+	}
 }
--- implementation/distributed/systems/executors/ServerExecutor.java
+++ implementation/distributed/systems/executors/ServerExecutor.java
@@ -1,55 +1,58 @@
 package distributed.systems.executors;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.SocketAddress;
-import java.nio.channels.AsynchronousSocketChannel;
-
 import distributed.systems.core.Constants;
 import distributed.systems.core.Message;
 import distributed.systems.core.ServerAndPorts;
-import distributed.systems.core.Socket;
 import distributed.systems.das.BattleField;
-import distributed.systems.server.Attachment;
 import distributed.systems.server.ServerSendReceive;
 
-public class ServerExecutor {
+public class ServerExecutor
+{
 
 	private ServerAndPorts sp;
-	private SocketAddress clientToServerAddr;
-	private SocketAddress serverToServerAddr;
 	private ServerSendReceive serverSendReceive;
 	private BattleField b;
 
-	ServerExecutor(int serverID) {
+	ServerExecutor(int serverID)
+	{
 		sp = Constants.SERVER_PORT[serverID];
 		b = BattleField.getBattleField();
 		b.init(this);
 		serverSendReceive = new ServerSendReceive(this);
 	}
-	
-	public static void main(String[] args) {
-		if(args.length != 1) {
+
+	public static void main(String[] args)
+	{
+		if (args.length != 1)
+		{
 			System.out.println("missing server port argument");
 			System.exit(1);
 		}
 		ServerExecutor se = new ServerExecutor(Integer.parseInt(args[0]));
 	}
-	
-	public void sendMessageToMany(Message msg) {
+
+	public void sendMessageToMany(Message msg)
+	{
 		serverSendReceive.sendToAll(msg);
 	}
 
-	public void sendMessageToOne(ServerAndPorts sp, Message msg) {
+	public void sendMessageToOne(ServerAndPorts sp, Message msg)
+	{
 		serverSendReceive.sendToOne(sp, msg);
 	}
-	
-	public void receiveMessage(Message msg) {
+
+	public void receiveMessage(Message msg)
+	{
 		b.receivedClientMessage(msg);
 	}
-	
-	public void receiveServerMessage(Message msg) {
+
+	public void receiveServerMessage(Message msg)
+	{
 		b.receivedServerMessage(msg);
 	}
 
+	public ServerAndPorts getServerPortData()
+	{
+		return this.sp;
+	}
 }
