9b52e434733b33f776a310d495d735b8344ceee7
Martijn van Beest <martijn.vanbeest@student.uva.nl>
2017-12-13 16:54:33 +0100

    repo cleanup

--- game_code/com/thirteen/shared/core/LocalSocket.java
+++
@@ -1,8 +0,0 @@
-package com.thirteen.shared.core;
-
-import com.thirteen.shared.core.Socket;
-public class LocalSocket extends Socket {
-
-	
-	
-}
--- game_code/com/thirteen/shared/units/base/UnitType.java
+++
@@ -1,8 +0,0 @@
-package com.thirteen.shared.units.base;
-
-public enum UnitType
-{
-	player,
-	dragon,
-	undefined,
-}
